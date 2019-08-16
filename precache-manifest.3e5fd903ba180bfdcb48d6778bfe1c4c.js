4ce51e2dddeda86ef9805a0163272c90b1c3cc31
brandonTsai <brandonya125@gmail.com>
2019-08-15 19:40:05 -0700

    Updates

---
+++ precache-manifest.3e5fd903ba180bfdcb48d6778bfe1c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c5adf63332d519c66e1b7ec0286303",
+    "url": "/robofriends/static/media/Cocogoose Pro Inline Trial.22c5adf6.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "9ad51ff9870b84795004",
+    "url": "/robofriends/static/js/main.8ed4cb30.chunk.js"
+  },
+  {
+    "revision": "4de42157371422d3ec34",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "9ad51ff9870b84795004",
+    "url": "/robofriends/static/css/main.1f5c1b6c.chunk.css"
+  },
+  {
+    "revision": "4de42157371422d3ec34",
+    "url": "/robofriends/static/css/2.8dfdbb6c.chunk.css"
+  },
+  {
+    "revision": "492eb82150156505535620813b4bb1f7",
+    "url": "/robofriends/index.html"
+  }
+];
